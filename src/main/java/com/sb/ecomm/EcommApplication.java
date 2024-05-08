27c0d9102c7d9e9848472454c2272de1301f3b8c
nlilaramani <nlilaramani@hotmail.com>
2024-05-08 11:50:15 -0500

    CORS config

--- src/main/java/com/sb/ecomm/EcommApplication.java
+++ src/main/java/com/sb/ecomm/EcommApplication.java
@@ -2,9 +2,27 @@ package com.sb.ecomm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcommApplication {
+
+		@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("*");
+
+			}
+
+		};
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommApplication.class, args);
